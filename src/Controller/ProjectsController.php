e085057ee2b5bc39c6643f41cf1c0cbc53d8e640
Sébastien GAUTIER <sgautier034@gmail.com>
2023-10-19 10:59:40 +0200

    Create Project ok

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\FormProjectsType;
 use App\Entity\Projects;
 use App\Repository\ProjectsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/projects')]
@@ -30,6 +34,25 @@ class ProjectsController extends AbstractController
         }
         return $this->render('projects/show.html.twig', [
             'projects' => $projects,
+        ]);
+    }
+
+    #[Route('/new', name: 'project_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $project = new Projects();
+        $form = $this->createForm(FormProjectsType::class, $project);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($project);
+            $em->flush();
+            return $this->redirectToRoute('projects_list');
+        }
+
+        return $this->render('projects/new.html.twig', [
+            'title' => 'Ajouter un project',
+            'form' => $form,
         ]);
     }
 }
