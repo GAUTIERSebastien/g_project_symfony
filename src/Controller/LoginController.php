45645ebad3a53505cf9ac12f64cffacc2d16b4d4
Sébastien GAUTIER <sgautier034@gmail.com>
2023-10-19 14:36:56 +0200

    Add Login => User

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        //Récupération des erreurs de connexion
+        $error = $authenticationUtils->getLastAuthenticationError();
+        //Récupéartion du nom d'utilisateur soumis s'il existe
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
